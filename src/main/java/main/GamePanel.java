4a92351d02a22720643156432acd9c38c9f2d2b5
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-09-21 22:58:28 -0400

    Creates drawInterval and next drawTime.
    - FPS set to 60.
    - run(): drawInterval 1000000000 / FPS.
    - run(): nextDrawTime nanotime() + drawInterval.
    - try/catch determines drawinterval/sleep/drawinterval.
    - tests: user controls move user block across screen.

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -45,6 +45,9 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         // Game Loop (core of game)
         while (gameThread != null) {
 
@@ -60,6 +63,22 @@ public class GamePanel extends JPanel implements Runnable {
 
             // 2 UPDATE: draw the screen with the updated information
             repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
