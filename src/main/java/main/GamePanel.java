046af65d49b244f5dec6512b75966472d382f475
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-09-21 22:06:26 -0400

    Added Thread to GamePanel for runtime.
    - run() method
    - startGameThread(): creats new Thread and uses start() method.
    - verified output message that verifies gameThread is running until game window is terminated by closing/exiting window.

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originialTileSize = 16; // 16 x 16 tile
@@ -15,11 +15,37 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixel
 
+    Thread gameThread;
+
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
     }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        // Game Loop (core of game)
+        while (gameThread != null) {
+
+            System.out.println("The game loop is running.");
+
+            // 1 UPDATE: update information such as character positions
+
+            // 2 UPDATE: draw the screen with the updated information
+        }
+    }
+
+
+
+
 
 }
