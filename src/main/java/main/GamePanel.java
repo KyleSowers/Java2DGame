409e95c9a4586d91aef899f3190917066f7ca1f9
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-09-21 22:40:50 -0400

    Adds key event handler and listener for user input.

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -15,14 +17,24 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixel
 
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
+    // Set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -36,7 +48,12 @@ public class GamePanel extends JPanel implements Runnable {
         // Game Loop (core of game)
         while (gameThread != null) {
 
+            // test initial run comment updating
 //            System.out.println("The game loop is running.");
+
+            // test time run output with nanoTime()
+//            long currentTime = System.nanoTime();
+//            System.out.println("current Time: " + currentTime);
 
             // 1 UPDATE: update information such as character positions
             update();
@@ -48,7 +65,17 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void update() {
 
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPresed == true) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
     }
+
 
     public void paintComponent(Graphics g) {
 
@@ -57,7 +84,7 @@ public class GamePanel extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
     }
 
--- src/main/java/main/KeyHandler.java
+++ src/main/java/main/KeyHandler.java
@@ -4,18 +4,48 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+
+    public boolean upPressed, downPresed, leftPressed, rightPressed;
+
     @Override
     public void keyTyped(KeyEvent e) {
-
+    // not using
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
 
+        int code = e.getKeyCode(); // returns number of key pressed
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPresed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
 
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPresed = false;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
     }
 }
